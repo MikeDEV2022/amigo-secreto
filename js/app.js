7df776c1c71c17987ac152728abadc0ab3b0814a
SrRaio <130098498+MikeDEV2022@users.noreply.github.com>
2025-03-09 16:06:24 -0300

    Adicionada a logica de implementação de nomes a lista

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
+let vet = [];
 function adicionar(){
     let amigo = document.getElementById("nome-amigo").value;
     let lista_Amigos = document.querySelector(".friends__container");
-    let createElement = document.createElement("p");
 
-    amigosArmazenados(amigo,lista_Amigos,createElement)
+    vet.push(amigo);
+
+    lista_Amigos.innerHTML += `<p id="lista-amigos">${vet}</p>`
     
-}
-function amigosArmazenados(amigo,lista_amigos,createElement){
-    createElement.innerHTML = `<p id="lista-amigos">${amigo}</p`
-    lista_amigos.append(createElement);
 }
