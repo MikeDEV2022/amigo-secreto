cc8fc7f8925a8772111ddd135a7dd2eee4bd34b1
SrRaio <130098498+MikeDEV2022@users.noreply.github.com>
2025-03-13 15:14:41 -0300

    Adicionado a logicca de limpar os nomes do vetor

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
-let lista_Teste = chamarLista()
+let lista_Teste = chamarLista();
 function chamarLista(){
     let lista_Teste = [];
-    return lista_Teste
+    return lista_Teste;
 }
 function adicionar(){
     let lista = document.querySelector(".friends__container");
     let amigo = document.getElementById("nome-amigo").value;
     
-    amigo = ` ${amigo}`
-    lista_Teste.push(amigo)
+    amigo = ` ${amigo}`;
+    lista_Teste.push(amigo);
 
-    lista.innerHTML = `<p id="lista-amigos">${lista_Teste}</p>`
+    lista.innerHTML = `<p id="lista-amigos">${lista_Teste}</p>`;
 }
 
 function reiniciar(){
-    document.querySelector(".friends__container").innerHTML = `<p id="lista-amigos"></p>`
-    document.getElementById("nome-amigo").value = ""
+    document.querySelector(".friends__container").innerHTML = `<p id="lista-amigos"></p>`;
+    document.getElementById("nome-amigo").value = "";
+    lista_Teste = [];
 }
