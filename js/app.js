433269ebe1d0ade39f8a7ddfe47c8e59b6b395a5
SrRaio <130098498+MikeDEV2022@users.noreply.github.com>
2025-03-13 13:52:29 -0300

    Adicionada a logica de implementação de nomes na lista e iniciado a função de reiniciar informações

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-let vet = [];
+let lista_Teste = chamarLista()
+function chamarLista(){
+    let lista_Teste = [];
+    return lista_Teste
+}
 function adicionar(){
+    let lista = document.querySelector(".friends__container");
     let amigo = document.getElementById("nome-amigo").value;
-    let lista_Amigos = document.querySelector(".friends__container");
-
-    vet.push(amigo);
-
-    lista_Amigos.innerHTML += `<p id="lista-amigos">${vet}</p>`
     
+    amigo = ` ${amigo}`
+    lista_Teste.push(amigo)
+
+    lista.innerHTML = `<p id="lista-amigos">${lista_Teste}</p>`
+}
+
+function reiniciar(){
+    document.querySelector(".friends__container").innerHTML = `<p id="lista-amigos"></p>`
+    document.getElementById("nome-amigo").value = ""
 }
